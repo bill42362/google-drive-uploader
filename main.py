abcb4ad25807e04ce0fe86a665e7307f629d839b
Bill42362 <bill42362@gmail.com>
2020-03-25 23:40:07 +0800

    Switch to use `pydrive` package to access google drive.

--- main.py
+++ main.py
@@ -1,57 +1,35 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 import sys
 import os
 import re
-import shlex, subprocess
-import httplib2
-import pprint
-import argparse
+import subprocess
 
-from apiclient.discovery import build
-from apiclient.http import MediaFileUpload
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client import tools
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
-dirname = os.path.dirname(os.path.realpath(sys.argv[0])) + '/'
+credentialsFilename = 'saved_user_creds.dat'
 
-USER_CREDS_FILE = dirname + "saved_user_creds.dat"
-# Download your credentials from the APIs Console
-SECRET_FILE = dirname + "secret_client.json"
-# Check https://developers.google.com/drive/scopes for all available scopes
-OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile(credentialsFilename)
+if gauth.credentials is None:
+    gauth.CommandLineAuth()
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+gauth.SaveCredentialsFile(credentialsFilename)
 
-storage = Storage(USER_CREDS_FILE)
-credentials = storage.get()
-if credentials is None or credentials.invalid:
-        flow = flow_from_clientsecrets(SECRET_FILE, OAUTH_SCOPE)
-        argparser = argparse.ArgumentParser(add_help=False)
-        argparser.add_argument(
-                '--noauth_local_webserver', action='store_true',
-                default=True, help='Do not run a local web server.'
-        )
-        argparser.add_argument(
-                '--logging_level', default='ERROR',
-                choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                help='Set the logging level of detail.'
-        )
-        flags = argparser.parse_args()
-        credentials = tools.run_flow(flow, storage, flags)
-http = credentials.authorize(httplib2.Http())
-drive_service = build("drive", "v2", http)
+drive = GoogleDrive(gauth)
 
 paths = sys.argv[1:]
 regProg = re.compile('^.*\/')
 for path in paths:
-        filename = regProg.sub('', path)
-        print 'Uploading ' + filename + ' ...'
-        mimetype = subprocess.Popen(
-                "file -b --mime-type " + path, shell=True,
-                stdout=subprocess.PIPE
-        ).communicate()[0]
-        media_body = MediaFileUpload(path, mimetype, resumable=True)
-        body = {
-                'title': filename
-        }
-        drive_service.files().insert(body=body, media_body=media_body).execute()
+        try:
+            filename = regProg.sub('', path)
+            print('Uploading ', filename, ' ...')
+            file = drive.CreateFile()
+            file.SetContentFile(filename)
+            file.Upload()
+            print('Upload ', filename, ' complete')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
