48a71b8e6ddeffe5b8b185a764f6f9cf8c55661b
Bill42362 <bill42362@gmail.com>
2020-03-26 01:07:35 +0800

    Enable folder id as argument.

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import sys
 import os
 import re
 import subprocess
+import argparse
 
 # pydrive reference: https://pythonhosted.org/PyDrive/pydrive.html
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-credentialsFilename = 'saved_user_creds.dat'
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dir-id', dest='dirId', help='destination google drive folder id')
+parser.add_argument('filepaths', nargs='+', help='files to be upload')
+args = parser.parse_args()
 
+# Authentication
 # ref: https://stackoverflow.com/a/24542604/2605764
+credentialsFilename = 'saved_user_creds.dat'
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile(credentialsFilename)
 if gauth.credentials is None:
@@ -20,16 +27,16 @@ elif gauth.access_token_expired:
 else:
     gauth.Authorize()
 gauth.SaveCredentialsFile(credentialsFilename)
-
 drive = GoogleDrive(gauth)
 
-paths = sys.argv[1:]
+# Upload files
+paths = args.filepaths
 regProg = re.compile('^.*\/')
 for path in paths:
         try:
             filename = regProg.sub('', path)
             print('Uploading ', filename, ' ...')
-            file = drive.CreateFile()
+            file = drive.CreateFile({ 'parents': [{ 'kind': 'drive#fileLink', 'id': args.dirId }] })
             file.SetContentFile(filename)
             file.Upload()
             print('Upload ', filename, ' complete')
